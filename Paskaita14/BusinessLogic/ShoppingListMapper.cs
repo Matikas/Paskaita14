752be467dc46056be14a47a77996bb1b4160b961
Marius Paškevičius <marius.paskevicius@yahoo.com>
2024-01-16 21:52:15 +0200

    Add project files.

---
+++ Paskaita14/BusinessLogic/ShoppingListMapper.cs
@@ -0,0 +1,22 @@
+﻿using Paskaita14.DTOs;
+using Paskaita14.Models;
+
+namespace Paskaita14.BusinessLogic
+{
+    public class ShoppingListMapper : IShoppingListMapper
+    {
+        public ShoppingList Map(CreateShoppingListDTO shoppingListDTO, int userId)
+        {
+            return new ShoppingList
+            {
+                Title = shoppingListDTO.Title,
+                UserId = userId,
+                Items = shoppingListDTO.Items.Select(x => new ShoppingListItem
+                {
+                    Title = x.Title,
+                    Quantity = x.Quantity
+                }).ToList()
+            };
+        }
+    }
+}
